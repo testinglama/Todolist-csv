cb4c4307a633fef495a7907a24995178e0f433c1
testinglama <aryankush0304@gmail.com>
2026-09-03 19:47:32 +0530

    added loop to continue the app untill the user want to exit

--- src/main.py
+++ src/main.py
@@ -12,16 +12,25 @@ ut.banner()
 print(colorama.Fore.WHITE)
 print("\n")
 print(colorama.Fore.LIGHTMAGENTA_EX+colorama.Style.BRIGHT+"     HELLO USER WELCOME TO TODO-LIST  :  "+"\n\n")
-print("     OPTIONS ARE :    "+colorama.Fore.YELLOW)
 
-print("ADD TASK : (1)\n")
-print("REMOVE TASK : (2)\n")
-print("EDIT TASK : (3)\n")
+while True:
+    print("     OPTIONS ARE :    "+colorama.Fore.YELLOW)
+    print("ADD TASK : (1)\n")
+    print("REMOVE TASK : (2)\n")
+    print("EDIT TASK : (3)\n")
 
-try:
     inp = int(input("WRITE YOUR CHOICE "))
-except:
-    print(colorama.Fore.RED+"some error occured"+ colorama.Fore.WHITE)
+
+    if inp == "" :
+        print("exiting the app...")
+        time.sleep(2)
+        print("exited the app!")
+        break
+
+
+
+
+
 
 
 
